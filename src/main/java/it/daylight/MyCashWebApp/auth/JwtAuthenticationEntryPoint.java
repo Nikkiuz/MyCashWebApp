578fa9b528ae96a36e23b48922df4cfa76372793
Niccolò Albanese <nick_albanese@virgilio.it>
2025-05-07 15:59:18 +0200

    fixing

--- src/main/java/it/daylight/MyCashWebApp/auth/JwtAuthenticationEntryPoint.java
+++ src/main/java/it/daylight/MyCashWebApp/auth/JwtAuthenticationEntryPoint.java
@@ -2,10 +2,9 @@ package it.daylight.MyCashWebApp.auth;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import javax.naming.AuthenticationException;
 import java.io.IOException;
 
 @Component
@@ -15,7 +14,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	public void commence(HttpServletRequest request,
 	                     HttpServletResponse response,
 	                     AuthenticationException authException) throws IOException {
-	                     response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
 
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Accesso non autorizzato");
 	}
 }
