e22b73c3481840fc7e6a92e810f35083b4328ceb
Niccolò Albanese <nick_albanese@virgilio.it>
2025-05-07 11:14:18 +0200

    Initial commit

---
+++ src/main/java/it/daylight/MyCashWebApp/MyCashWebAppApplication.java
@@ -0,0 +1,13 @@
+package it.daylight.MyCashWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCashWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCashWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/daylight/MyCashWebApp/MyCashWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.daylight.MyCashWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCashWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
