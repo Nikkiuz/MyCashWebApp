e88de102ce5963aac311ba3d4bc4a85129c45065
Niccolò Albanese <nick_albanese@virgilio.it>
2025-05-08 11:57:38 +0200

    Update OutcomeContoller.java

--- src/main/java/it/daylight/MyCashWebApp/controller/OutcomeContoller.java
+++ src/main/java/it/daylight/MyCashWebApp/controller/OutcomeContoller.java
@@ -1,14 +1,9 @@
 package it.daylight.MyCashWebApp.controller;
 
+import it.daylight.MyCashWebApp.dto.OutcomeRequestDTO;
+import it.daylight.MyCashWebApp.entity.OutcomeCategories;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -19,7 +14,7 @@ import it.daylight.MyCashWebApp.entity.User;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/outcome")
+@RequestMapping("/api/outcomes")
 @RequiredArgsConstructor
 public class OutcomeContoller {
 
